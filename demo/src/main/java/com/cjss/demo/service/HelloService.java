a726c510c1cf6ae3c8839a39b0569cdd99c350fe
Srikanth Reddy Banthu <35705507+SrikanthReddyBanthu@users.noreply.github.com>
2021-10-14 16:25:42 +0530

    Spring Boot Demo 1

---
+++ demo/src/main/java/com/cjss/demo/service/HelloService.java
@@ -0,0 +1,16 @@
+package com.cjss.demo.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String getDetails() {
+        return "Dummy details";
+    }
+
+    public Integer add(int a, int b) {
+        return "Dummy details";
+    }
+}
